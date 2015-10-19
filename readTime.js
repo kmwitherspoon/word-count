b4fc1a90fcd8c28808c0c22db60a4920b0899253
kmwitherspoon <kellee.morgan.witherspoon@gmail.com>
2015-10-19 15:18:28 -0400

    After clean up

--- readTime.js
+++ readTime.js
@@ -9,25 +9,26 @@ console.log(myParaArray);
 // Thanks Paul Draper for the whitespace hint
 
 function wordCount(wpm) {
-  var myPara = document.body.textContent;
-  var myParaArray = myPara.split(" ");
-  var myParaArrayPara = myParaArray.filter(function(str) {
-    return /\S/.test(str);
-})
-var wpm = 230;
-var letsRead = myParaArrayPara.length / wpm;
-letsRead = Math.round(letsRead);
-timeToRead = document.getElementsByClassName('time-to-read'); timeToRead[0].innerHTML = letsRead + " minutes to read";
+    var myPara = document.body.textContent;
+    var myParaArray = myPara.split(" ");
+    var myParaArrayPara = myParaArray.filter(function(str) {
+        return /\S/.test(str);
+    })
+    var wpm = 230;
+    var letsRead = myParaArrayPara.length / wpm;
+    letsRead = Math.round(letsRead);
+    timeToRead = document.getElementsByClassName('time-to-read');
+    timeToRead[0].innerHTML = letsRead + " minutes to read";
 };
 
-
 function wordCountUser(wpm) {
-  var myPara = document.body.textContent;
-  var myParaArray = myPara.split(" ");
-  var myParaArrayPara = myParaArray.filter(function(str) {
-    return /\S/.test(str);
-  })
-  var letsRead = myParaArrayPara.length / wpm;
-  letsRead = Math.round(letsRead);
-  timeToRead = document.getElementsByClassName('time-to-read'); timeToRead[0].innerHTML = letsRead + " minutes to read";
-  };
+    var myPara = document.body.textContent;
+    var myParaArray = myPara.split(" ");
+    var myParaArrayPara = myParaArray.filter(function(str) {
+        return /\S/.test(str);
+    })
+    var letsRead = myParaArrayPara.length / wpm;
+    letsRead = Math.round(letsRead);
+    timeToRead = document.getElementsByClassName('time-to-read');
+    timeToRead[0].innerHTML = letsRead + " minutes to read";
+};
