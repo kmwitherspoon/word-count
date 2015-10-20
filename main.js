d27713a1e4088ec25977cc9c1a4eee1291289739
kmwitherspoon <kellee.morgan.witherspoon@gmail.com>
2015-10-20 09:42:45 -0400

    Made some edits to make the files more organized

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 
 
+var myPara = document.body.textContent;
+var myParaArray = myPara.split(" ");
+var myParaArrayPara = myParaArray.filter(function(str) {
+    return /\S/.test(str);
+});
+
 wordCount();
--- readTime.js
+++ readTime.js
@@ -6,14 +6,10 @@ var myParaArray = myPara / 200;
 console.log(myParaArray);
 */
 
+
 // Thanks Paul Draper for the whitespace hint
 
 function wordCount(wpm) {
-    var myPara = document.body.textContent;
-    var myParaArray = myPara.split(" ");
-    var myParaArrayPara = myParaArray.filter(function(str) {
-        return /\S/.test(str);
-    })
     var wpm = 230;
     var letsRead = myParaArrayPara.length / wpm;
     letsRead = Math.round(letsRead);
@@ -21,12 +17,8 @@ function wordCount(wpm) {
     timeToRead[0].innerHTML = letsRead + " minutes to read";
 };
 
+
 function wordCountUser(wpm) {
-    var myPara = document.body.textContent;
-    var myParaArray = myPara.split(" ");
-    var myParaArrayPara = myParaArray.filter(function(str) {
-        return /\S/.test(str);
-    })
     var letsRead = myParaArrayPara.length / wpm;
     letsRead = Math.round(letsRead);
     timeToRead = document.getElementsByClassName('time-to-read');
